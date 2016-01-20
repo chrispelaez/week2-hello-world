25e9510610dcbb102ca05bcb8e165c6462b32a4d
chrispelaez <cpelaez5@mail.valenciacollege.edu>
2016-01-20 10:15:06 -0500

    Update helloworld.py
    I checked this morning for the changes I made yesterday but I couldn't find the changes to the file so I am submitting the assignment again.

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,57 @@
-# replace the contents of this comment with your full name
 
-# write a program that:
-# 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
-# 3. displays the greeting in the chosen language
-# 4. exits
+# Chris Pelaez
 
-# make sure that your code contains comments explaining your logic!
+print('Hello. Please choose the language you would like.')
+
+# Display options
+print('Press 1: Hungarian.')
+print('Press 2: Italian.')
+print('Press 3: Russian.')
+
+# create option variable to recieve input from user.
+option = input()
+
+# print out appropriate language when option matches 1,2, or 3.
+if option == '1':
+    print ('Jó reggelt!')
+elif option == '2':
+    print ('Ciao')
+elif option == '3':
+    print ('Zdravstvuyte')
+
+print('Hello. Please choose the language you would like.')
+
+# Display options
+print('Press 1: Hungarian.')
+print('Press 2: Italian.')
+print('Press 3: Russian.')
+
+# create option variable to recieve input from user.
+option = input()
+
+# print out appropriate language when option matches 1,2, or 3.
+if option == '1':
+    print ('Jó reggelt!')
+elif option == '2':
+    print ('Ciao')
+elif option == '3':
+    print ('Zdravstvuyte')
+
+print('Hello. Please choose the language you would like.')
+
+# Display options
+print('Press 1: Hungarian.')
+print('Press 2: Italian.')
+print('Press 3: Russian.')
+
+# create option variable to recieve input from user.
+option = input()
+
+# print out appropriate language when option matches 1,2, or 3.
+if option == '1':
+    print ('Jó reggelt!')
+elif option == '2':
+    print ('Ciao')
+elif option == '3':
+    print ('Zdravstvuyte')
 
